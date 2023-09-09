dd4c6f6ade4739e7f4be5f44d8f0fd7e543bc4f0
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-09-09 15:01:04 -0600

    answered questions 1-4 in gitInfo.js

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,8 @@
 */
 
 //CODE HERE
+let gitDefinition =
+  "Git is a program that tracks changes to files.  You dicide which folders you'd like to track, and have Git create a repository within that folder.  Then it will track any changes made within that folder, and sub-folders going forward.  You can commit your files to the git repository as often as you like; and each time you do, that commit functions as a kind of save point that you can always go back to.  Git is a really powerful way to store a complete version history of many files.";
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+let gitHubDefinition =
+  "GitHub is a popular, off-site (remote), cloud-based location for storing Git repositories.  When you push your Gits to GitHub, it stores a copy of the complete Git, with version history.  This means you still have access to your data, even if you no longer have access to your computer.  GitHub can also provides a powerful way to collaborate and share your data with others.  GitHub is a strong resource used by programmers across the world.";
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -26,6 +30,8 @@
 */
 
 //CODE HERE
+let gitInitDefinition =
+  "'Git init' is the command you execute to create a Git Repository.  You execute this command in the terminal, from within the folder directory that you want to track.  it should be followed up with the 'git add .' and 'git commit' commands.  The '.' in 'git add .' indicates 'all files and folders'.  Using this command will stage all the files and folders contained in the current directory, making them ready to be committed.  You will then need to commit these files to the Git with the 'git commit' command.  Doing so adds the current version of the files to the Git repository, so you'll want to do it often.  None of this can happen unless you initiate the git repository in the first place, which is done with 'git init'.";
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -35,6 +41,9 @@
 
 //CODE HERE
 
+let gitCloneDefinition =
+  "'git clone' creates a local copy of a remote git repository, placing the copy on your own computer.  By default, the local copy is connected to the remote copy - if you have the necessary permissions.  This allows you to easily syncronize changes to and from the remote repository.";
+
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +51,7 @@
 */
 
 //CODE HERE
+let gitStatusDefinition = "";
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -64,7 +74,6 @@
 */
 
 //CODE HERE
-
 
 //////////////////PROBLEM 8////////////////////
 /*
